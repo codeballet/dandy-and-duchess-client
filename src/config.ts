0fd707ff37a75e954f3d6601d517223703d2a9c0
johan <stjernholm@msn.com>
2020-02-06 09:51:10 +0800

    Refactor: change config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '91bgnc1ydd'
-export const apiEndpoint = `https://${apiId}.execute-api.ap-east-1.amazonaws.com/dev`
+const apiId = 'pa4n00wl9d'
+const region = 'ap-northeast-1'
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
