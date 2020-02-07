0bf36ce0f55a0b38d2a286082bd07c32871c0d2d
johan <stjernholm@msn.com>
2020-02-07 14:46:56 +0800

    Refactor: Home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,10 +8,28 @@ import {
 const Home = (props: any) => {
   return (
     <Grid padded centered>
-      <Grid.Column verticalAlign="middle">
-          <Header as="h1">Dandy and Duchess</Header>
-          <Image centered alt="Home picture" src={props.image} size="large" />
-      </Grid.Column>
+      <Grid.Row>
+        <Grid.Column width={12} verticalAlign="middle">
+            <Header as="h1">Dandy and Duchess</Header>
+            <Image centered alt="Home picture" src={props.image} />
+
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={12}>
+          <div style={{ textAlign: "justify" }}>
+            <p>Dancy and Duchess provides space for you to explore 
+              and advance your interest and knowledge in how to style 
+              style your life in a contemporary fashion.
+            </p>
+            <p>To get started, log in to explore the Ideas section, 
+              where you can find articles about various aspects of 
+              style and living. In the My Zone section, you can 
+              start to build up your own collection of ideas.</p>
+            <p>Enjoy!</p>
+          </div>
+        </Grid.Column>
+      </Grid.Row>
     </Grid>
   )
 }
